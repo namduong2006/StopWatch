f85ee633ca3137b58c3b603f18f2f7e95601b24a
namduong2006 <129411334+namduong2006@users.noreply.github.com>
2023-04-23 14:39:32 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.OutputEncoding = Encoding.UTF8;           
+            StopWatch time = new StopWatch();
+            time.Start();        
+            Selectionsort();            
+            time.Stop();
+            double Elapsed= time.GetElapsedTime();
+            Console.WriteLine("Thời gian thực thi thuật toán sắp xếp chọn: " + Elapsed.ToString("0") + "ms");
+            Console.ReadLine();           
+        }
+        static void Selectionsort()
+        {
+            Random rand = new Random();
+            int[] List = new int[100000];
+            for(int i = 0; i < List.Length; i++)
+            {
+                List[i] = rand.Next();
+            }
+            for (int i = 0; i < List.Length - 1 - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < List.Length; j++)
+                {
+                    if (List[j] < List[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                int temp = List[i];
+                List[i] = List[minIndex];
+                List[minIndex] = temp;
+            }            
+        }       
+    }
+}
---
+++ StopWatch.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Diagnostics;
+using System.Runtime.InteropServices.ComTypes;
+
+
+namespace Time
+{
+    internal class StopWatch
+    {
+        public DateTime startTime;       
+        public DateTime endTime;
+        public void SelectionSortTimer()
+        {
+            startTime = DateTime.Now;
+        }
+        public DateTime StartTime()
+        {
+            return startTime;
+        }
+        public DateTime EndTime() 
+        {           
+             return endTime; 
+        }
+        public void Start()
+        {
+            startTime = DateTime.Now;
+            string Start = startTime.ToString("dd-MM-yyyy HH:mm:ss.fff");
+            Console.WriteLine("Thời gian Start: " + Start);
+        }
+        public void Stop()
+        {
+            endTime = DateTime.Now;
+            string End = endTime.ToString("dd-mm-yyyy hh:mm:ss.fff");
+            Console.WriteLine("thời gian stop: " + End);
+        }
+        public double GetElapsedTime()
+        {
+            TimeSpan elapsed = endTime-startTime;            
+            return elapsed.TotalMilliseconds;
+        }
+    }
+    
+}
+ 
